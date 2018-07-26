0cef3fcfc6feeea6ff3e9615d3c3d802c460954a
AlexLigre <36110246+AlexLigre@users.noreply.github.com>
2018-07-26 12:29:39 +0300

    Changes in MainReflection

--- src/com/alexligre/resumedb/MainReflection.java
+++ src/com/alexligre/resumedb/MainReflection.java
@@ -7,25 +7,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
-        Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
-        field.setAccessible(true);
-        System.out.println(field.getName());
-        System.out.println(field.get(r));
-        field.set(r, "new123123");
-        System.out.println(r);
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Resume resume = new Resume();
+        System.out.println(resume);
 
-        System.out.println("********");
-        //invoke r.toString via reflection
-
-        Method []methods = r.getClass().getMethods();
-        for (Method m:methods) {
-            if(m.getName().equals("toString")){
-                System.out.println(m.invoke(r));
-                break;
-            }
-        }
-
+        Method method = resume.getClass().getMethod("toString");
+        System.out.println(method.invoke(resume));
     }
 }
