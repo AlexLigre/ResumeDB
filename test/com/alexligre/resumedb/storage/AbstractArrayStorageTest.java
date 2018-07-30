887ab9e282f943611af337ff8fa5629650d77019
AlexLigre <36110246+AlexLigre@users.noreply.github.com>
2018-07-30 13:49:18 +0300

    Changes in tests' class

--- test/com/alexligre/resumedb/storage/AbstractArrayStorageTest.java
+++ test/com/alexligre/resumedb/storage/AbstractArrayStorageTest.java
@@ -6,19 +6,14 @@ import com.alexligre.resumedb.exception.StorageException;
 import com.alexligre.resumedb.model.Resume;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public abstract class AbstractArrayStorageTest {
 
     protected static final int NUM_OF_RESUMES = 5;
 
     Storage ARRAY_STORAGE;
-    Resume[] rArr = {new Resume("id#1"),
-            new Resume("id#2"),
-            new Resume("id#3"),
-            new Resume("id#4"),
-            new Resume("id#5"),};
+    Resume tResume = new Resume("id#125");
 
     @Test
     public void size() {
@@ -33,24 +28,28 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void save() {
-        ARRAY_STORAGE.save(new Resume("id#125"));
+        ARRAY_STORAGE.save(tResume);
         assertEquals("id#125", ARRAY_STORAGE.get("id#125").getUuid());
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void alreadyExists() {
+        ARRAY_STORAGE.save(new Resume("id#" + (ARRAY_STORAGE.size() - 1)));
+    }
+
+    @Test
+    public void overFlow() {
+        try{
+            ARRAY_STORAGE.save(new Resume("id#" + 1000));
+        }catch (StorageException e){
+            assertEquals("Storage is full", e.getMessage());
+        }
+
     }
 
     @Test
     public void get() {
         assertEquals("id#5", ARRAY_STORAGE.get("id#5").getUuid());
-    }
-
-    @Test
-    public void getAll() {
-        assertArrayEquals(rArr, ARRAY_STORAGE.getAll());
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void update() {
-        ARRAY_STORAGE.update(new Resume("id#125"));
-        assertEquals("id#125", ARRAY_STORAGE.get("id#125").getUuid());
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -59,13 +58,20 @@ public abstract class AbstractArrayStorageTest {
         ARRAY_STORAGE.get("id#3");
     }
 
-    @Test(expected = StorageException.class)
-    public void overFlow() {
-        ARRAY_STORAGE.save(new Resume("id#" + ARRAY_STORAGE.size()));
+    @Test
+    public void getAll() {
+        Resume[] rArr = {
+                new Resume("id#1"),
+                new Resume("id#2"),
+                new Resume("id#3"),
+                new Resume("id#4"),
+                new Resume("id#5"),};
+        assertArrayEquals(rArr, ARRAY_STORAGE.getAll());
     }
 
-    @Test(expected = ExistStorageException.class)
-    public void alreadyExists(){
-        ARRAY_STORAGE.save(new Resume("id#" + (ARRAY_STORAGE.size()-1)));
+    @Test(expected = NotExistStorageException.class)
+    public void update() {
+        ARRAY_STORAGE.update(tResume);
+        assertEquals("id#125", ARRAY_STORAGE.get("id#125").getUuid());
     }
 }
