ec5076073ce45e18bae50dbb4e8d81f1854b7281
AlexLigre <36110246+AlexLigre@users.noreply.github.com>
2018-09-30 20:33:08 +0300

    #5 Updated tests

--- test/com/alexligre/resumedb/storage/AbstractArrayStorageTest.java
+++ test/com/alexligre/resumedb/storage/AbstractArrayStorageTest.java
@@ -2,14 +2,15 @@ package com.alexligre.resumedb.storage;
 
 import com.alexligre.resumedb.exception.ExistStorageException;
 import com.alexligre.resumedb.exception.NotExistStorageException;
+import com.alexligre.resumedb.exception.StorageException;
 import com.alexligre.resumedb.model.Resume;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static com.alexligre.resumedb.storage.AbstractArrayStorage.STORAGE_LIMIT;
+import static org.junit.Assert.*;
 
-public abstract class AbstractStorageTest {
+public abstract class AbstractArrayStorageTest {
     Storage storage;
 
     Resume resume_1 = new Resume("id#1");
@@ -17,7 +18,7 @@ public abstract class AbstractStorageTest {
     Resume resume_3 = new Resume("id#3");
     Resume resume_4 = new Resume("id#4");
 
-    public AbstractStorageTest(Storage storage) {
+    public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
 
@@ -72,6 +73,18 @@ public abstract class AbstractStorageTest {
     @Test(expected = ExistStorageException.class)
     public void saveExistStorageException() {
         storage.save(resume_3);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveStorageException() {
+        try {
+            for (int i = storage.size(); i < STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (Exception e) {
+            fail("Test was failed");
+        }
+        storage.save(new Resume());
     }
 
     @Test
--- test/com/alexligre/resumedb/storage/MapStorageTest.java
+++ test/com/alexligre/resumedb/storage/MapStorageTest.java
@@ -1,5 +1,7 @@
 package com.alexligre.resumedb.storage;
 
-public class MapStorageTest {
-
+public class MapStorageTest extends AbstractStorageTest {
+    public MapStorageTest() {
+        super(new MapStorage());
+    }
 }
