038136b65900697b1482c196350626b07b1b7978
AlexLigre <36110246+AlexLigre@users.noreply.github.com>
2018-10-07 23:09:25 +0300

    #6 Added SuiteClass

---
+++ test/com/alexligre/resumedb/storage/SuiteClass.java
@@ -0,0 +1,8 @@
+package com.alexligre.resumedb.storage;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, MapStorageTest.class, ListStorageTest.class})
+public class SuiteClass {
+}
